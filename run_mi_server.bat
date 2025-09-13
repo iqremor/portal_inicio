f18fbc2e4c4be456e855146a5dfe37fd22e2177d
Romer A. Zambrano Carvajal <rzambranoc@unal.edu.co>
2025-09-13 14:04:10 -0500

    integración del backend

---
+++ run_mi_server.bat
@@ -0,0 +1,4 @@
+@echo off
+call "C:\Users\dqu_r\Documents\proyects\Fundamentos\Backend\python\mi_server\mi_server\Scripts\activate"
+python "C:\Users\dqu_r\Documents\proyects\Fundamentos\Backend\python\mi_server\main.py" run
+exit
